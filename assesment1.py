c2804d9284d5c142a7569e06549d62f395503c1c
Urwa_Kausar <urwakausar55@gmail.com>
2026-04-04 00:26:11 +0500

    Add files via upload

---
+++ assesment1.py
@@ -0,0 +1,25 @@
+#write a program to input 2 numbers & print their sum.
+a=int(input("Enter a number:"))
+b=int(input("Again enter a number:"))
+sum=(a+b)
+print("sum is ",sum)
+#done
+
+#write a program to input side of square & print its area.
+c=int(input("Enter the value of side:"))
+area=c**2
+print(area)
+#done
+
+#write a program to input 2 floating point numbers & print their AVG.
+d=float(input("Enter a number in decimal:"))
+e=float(input("Enter again a number in decimal:"))
+#f=(d+e) OR 
+AVG=((d+e)/2)
+print(AVG)
+#done
+
+#write a program to input 2 int number, a and b.Print True if a is greater than or equal to b.If not print False
+a=int(input("Enter a number:"))
+b=int(input("Enter another numner:"))
+print(a<=b)
